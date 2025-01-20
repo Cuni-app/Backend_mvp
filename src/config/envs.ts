c27320aead9aecdcdf0508de771b0b386dfc098c
Sebastián Guzmán <20200981@aloe.ulima.edu.pe>
2025-01-20 00:37:27 -0500

    Adaptadores de bcrypt y jwt

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -2,5 +2,7 @@ import "dotenv/config"
 import { get } from "env-var"
 
 export const envs = {
-    PORT: get('PORT').required().asPortNumber()
+    PORT: get('PORT').required().asPortNumber(),
+    JWT_SECRET: get('JWT_SECRET').required().asString()
+
 }
