b0fb9cfbee81f2c374541d9d3afaae8a4da1335f
Sideron <guardian2012cave@gmail.com>
2025-01-23 11:22:25 -0500

    pregunta endpoints

--- src/presentation/Preguntas/controller.ts
+++ src/presentation/Preguntas/controller.ts
@@ -1,5 +1,19 @@
+import { Request, Response } from "express";
 
 
 export class PreguntasController {
     constructor() {}
+
+    public crearPregunta = (req: Request, res: Response) => {
+        const {categoria, enunciado, imagen_url, solucion_url} = req.body
+
+    }
+
+    public obtenerPregunta = (req: Request, res: Response) => {
+
+    }
+
+    public eliminarPregunta = (req: Request, res: Response) => {
+
+    }
 }
--- src/presentation/Preguntas/routes.ts
+++ src/presentation/Preguntas/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { PreguntasController } from './controller';
 
 
 
@@ -9,9 +10,14 @@ export class PreguntaRoutes {
   static get routes(): Router {
 
     const router = Router();
+    const controller = new PreguntasController()
     
     // Definir las rutas
     // router.use('/api/algo', /*TodoRoutes.routes */ );
+    router.post("/", (req,res) => controller.crearPregunta(req,res))
+    router.get("/:id", (req,res) => controller.obtenerPregunta(req,res))
+    router.delete("/:id", (req,res) => controller.eliminarPregunta(req,res))
+
     return router;
   }
 
