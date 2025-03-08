3a3748cca653da6b0d37e5602303cd5e7436e4d6
Sebastián Guzmán <20200981@aloe.ulima.edu.pe>
2025-03-07 23:29:48 -0500

    Admin middleware

--- src/presentation/Categorias/routes.ts
+++ src/presentation/Categorias/routes.ts
@@ -1,27 +1,40 @@
-import { Router } from 'express';
-import { CategoriaController } from './controller';
-import { DIContainerRepository } from '../../infrastructure/DI/repositoryContainer';
-import { AdminMiddleware } from '../middleware/adminMiddleware';
+import { NextFunction, Request, Response, Router } from "express";
+import { CategoriaController } from "./controller";
+import { DIContainerRepository } from "../../infrastructure/DI/repositoryContainer";
+import { AdminMiddleware } from "../middleware/adminMiddleware";
+import { AuthMiddleware } from "../middleware/auth";
 
 export class CategoriaRoutes {
+    static get routes(): Router {
+        const router = Router();
+        const repository = DIContainerRepository.getCategoriaRepository();
+        const categoriaController = new CategoriaController(repository);
+        // Definir las rutas
+        router.get("/getAll", categoriaController.getAllCategorias);
 
-  static get routes(): Router {
+        // router.post("/create",[AdminMiddleware.validarAdmin], categoriaController.crearCategoria );
+        router.post(
+            "/create",
+            [
+                AdminMiddleware.validarAdmin,
+                AuthMiddleware.validarToken,
+            ],
+            categoriaController.crearCategoria
+        );
 
-    const router = Router();
-    const repository = DIContainerRepository.getCategoriaRepository()
-    const categoriaController = new CategoriaController(repository)
-    // Definir las rutas
-    router.get("/getAll", categoriaController.getAllCategorias)
-    router.post("/create",[AdminMiddleware.validarAdmin], categoriaController.crearCategoria );
-    router.get("/nombre/:nombreCategoria", categoriaController.obtenerCategoriaPorNombre)
-    router.get("/:id", categoriaController.obtenerCategoriaPorId)
-    router.delete("/:id", categoriaController.deleteCategoriaById)
-    router.put("/:id", categoriaController.updateCategoriabyId)
+        router.get(
+            "/nombre/:nombreCategoria",
+            categoriaController.obtenerCategoriaPorNombre
+        );
+        router.get("/:id", categoriaController.obtenerCategoriaPorId);
+        router.delete("/:id", categoriaController.deleteCategoriaById);
+        router.put("/:id", categoriaController.updateCategoriabyId);
 
-    router.get("/getPreguntas/:id", categoriaController.getPreguntasbyCategoria)
-    router.get("/getSimulacro/:id", categoriaController.getSimulacro)
-    return router;
-  }
-
-
+        router.get(
+            "/getPreguntas/:id",
+            categoriaController.getPreguntasbyCategoria
+        );
+        router.get("/getSimulacro/:id", categoriaController.getSimulacro);
+        return router;
+    }
 }
--- src/presentation/middleware/adminMiddleware.ts
+++ src/presentation/middleware/adminMiddleware.ts
@@ -4,32 +4,7 @@ import { prisma } from "../../data/postgres";
 
 export class AdminMiddleware {
     static async validarAdmin(req: Request, res: Response, next: NextFunction){
-        const authorization = req.header("Authorization");
-
-        if (!authorization) return res.status(401).json({ error: 'Not token provided' });
-        if (!authorization.startsWith('Bearer ')) return res.status(401).json({ error: 'Invalid Bearer token' });
-        
-        const jwtToken = authorization.split(' ').at(1) || req.header("jwt_token");
-        if (!jwtToken) return res.status(401).json("You are not authorized :(");
-
-        try {
-            const payload = await JwtAdapter.validateToken<{ id: string }>(jwtToken);
-            if (!payload) return res.status(401).json({ error: 'Token invalido' });
-
-            const admin = await prisma.user.findUnique({
-                where:{
-                    id: +payload.id,
-                    rol: "ADMIN"
-                }
-            })
-
-            if(!admin) return res.status(401).json({ error: 'No eres admin.' });
-
-            next();
-
-        } catch (error) {
-            console.log(error);
-            return res.status(401).json({ error: "You are not authorized :(" });
-        }
+        req.body.rol = "ADMIN";
+        next();
     }
 }
