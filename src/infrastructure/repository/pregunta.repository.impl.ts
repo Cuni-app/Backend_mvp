653ba18d668bc2b8b7f3cb18ad16062fc2bb9996
Sebastián Guzmán <s.guzman.1335@gmail.com>
2025-04-29 12:48:07 -0500

    primeros cambios simplificar ds repos usecases

--- src/infrastructure/repository/pregunta.repository.impl.ts
+++ src/infrastructure/repository/pregunta.repository.impl.ts
@@ -1,23 +1,20 @@
 import { CreatePreguntaDTO, PreguntaDatasource, PreguntaEntity, PreguntaRepository, RespuestaEntity, UpdatePreguntaDTO } from "../../domain";
 
-export class PreguntaRepositoryImpl implements PreguntaRepository{
-    constructor(
-        private readonly dataSource: PreguntaDatasource
-    ){}
-    getRespuestasbyIdPregunta(id: number): Promise<PreguntaEntity> {
-        return this.dataSource.getRespuestasbyIdPregunta(id)
-    }
-    create(createPreguntaDTO: CreatePreguntaDTO): Promise<PreguntaEntity> {
-        return this.dataSource.create(createPreguntaDTO)
-    }
-    getById(id: number): Promise<PreguntaEntity> {
-        return this.dataSource.getById(id)
-    }
-    updateById(updatePreguntaDTO: UpdatePreguntaDTO): Promise<PreguntaEntity> {
-        return this.dataSource.updateById(updatePreguntaDTO)
-    }
-    deleteById(id: number): Promise<PreguntaEntity> {
-        return this.dataSource.deleteById(id)
-    }
-    
+export class PreguntaRepositoryImpl implements PreguntaRepository {
+  constructor(
+    private readonly dataSource: PreguntaDatasource
+  ) { }
+  create(createPreguntaDTO: CreatePreguntaDTO): Promise<PreguntaEntity> {
+    return this.dataSource.create(createPreguntaDTO)
+  }
+  getById(id: number): Promise<PreguntaEntity> {
+    return this.dataSource.getById(id)
+  }
+  updateById(updatePreguntaDTO: UpdatePreguntaDTO): Promise<PreguntaEntity> {
+    return this.dataSource.updateById(updatePreguntaDTO)
+  }
+  deleteById(id: number): Promise<PreguntaEntity> {
+    return this.dataSource.deleteById(id)
+  }
+
 }
